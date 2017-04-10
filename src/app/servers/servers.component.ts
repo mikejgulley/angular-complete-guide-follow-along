bbfd6cf4f5199890be33ddc41fa7c4f8a52a8f31
Michael Gulley <mgulley.mograph@gmail.com>
2017-04-10 14:23:40 -0400

    working on making new components

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers', // <app-servers></app-servers>
+  //selector: '[app-servers]', // select as attr --> <div app-servers></div>
+  //selector: '.app-servers', // select as class --> <div class="app-servers"></div> // CANNOT use ID as selector
+  //templateUrl: './servers.component.html',
+  template: `
+      <app-server></app-server>
+      <app-server></app-server>
+    `,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
