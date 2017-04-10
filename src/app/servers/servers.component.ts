0ca71f839a7f4f5e45a02c474f7511e0e36865cb
Michael Gulley <mgulley.mograph@gmail.com>
2017-04-10 16:01:14 -0400

    data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,18 +4,33 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers', // <app-servers></app-servers>
   //selector: '[app-servers]', // select as attr --> <div app-servers></div>
   //selector: '.app-servers', // select as class --> <div class="app-servers"></div> // CANNOT use ID as selector
-  //templateUrl: './servers.component.html',
-  template: `
-      <app-server></app-server>
-      <app-server></app-server>
-    `,
+  templateUrl: './servers.component.html',
+  // template: `
+  //     <app-server></app-server>
+  //     <app-server></app-server>
+  //   `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer'
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was Created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
